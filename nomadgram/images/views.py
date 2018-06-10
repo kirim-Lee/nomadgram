4c84f2a55208b08d520ae1313fabf61c54c885de
kirim-Lee <uthi1004@gmail.com>
2018-06-10 09:10:56 +0900

    1-53까지

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -31,7 +31,7 @@ class Feed(APIView):
 
 class LikeImage(APIView):
 
-    def get(self, request, image_id, format=None):
+    def post(self, request, image_id, format=None):
 
         user = request.user
 
@@ -46,9 +46,7 @@ class LikeImage(APIView):
                 image = found_image
             )
 
-            preexisting_like.delete()
-
-            return Response(status = status.HTTP_204_NO_CONTENT)
+            return Response(status = status.HTTP_304_NOT_MODIFIED)
 
         except models.Like.DoesNotExist:
 
@@ -60,3 +58,70 @@ class LikeImage(APIView):
             new_like.save()
 
             return Response(status = status.HTTP_201_CREATED)
+
+class UnLikeImage(APIView):
+
+    def delete(self, request, image_id, format=None):
+
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+            
+        try:
+            preexisting_like = models.Like.objects.get(
+                creator = user,
+                image = found_image
+            )
+            
+            preexisting_like.delete()
+
+            return Response(status = status.HTTP_204_NO_CONTENT)
+
+        except models.Like.DoesNotExist:
+
+            return Response(status = status.HTTP_304_NOT_MODIFIED)
+
+
+class CommentOnImage(APIView):
+
+    def post(self, request, image_id, format=None):
+
+        user = request.user
+
+        try:
+            found_image = models.Image.objects.get(id=image_id)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+            
+
+        serializer = serializers.CommentSerializer(data=request.data)
+
+        if serializer.is_valid():
+            
+            serializer.save(creator = user, image = found_image)
+
+            return Response(data=serializer.data, status = status.HTTP_201_CREATED)
+
+        else:
+
+            return Response(data=serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+
+class Comment(APIView):
+
+    def delete(self, request, comment_id, format=None):
+        
+        user = request.user
+
+        try:
+            comment=models.Comment.objects.get(id=comment_id,creator=user)
+            comment.delete()
+            return Response(status = status.HTTP_204_NO_CONTENT)
+
+        except models.Comment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        
--- nomadgram/users/urls.py
+++ nomadgram/users/urls.py
@@ -4,12 +4,34 @@ from . import views
 
 app_name = "users"
 urlpatterns = [
-    url(regex=r"^$", view=views.UserListView.as_view(), name="list"),
-    url(regex=r"^~redirect/$", view=views.UserRedirectView.as_view(), name="redirect"),
-    url(regex=r"^~update/$", view=views.UserUpdateView.as_view(), name="update"),
     url(
-        regex=r"^(?P<username>[\w.@+-]+)/$",
-        view=views.UserDetailView.as_view(),
-        name="detail",
+        regex=r"^explore/$", 
+        view=views.ExploreUsers.as_view(), 
+        name="expolore_users"
+    ),
+    url(
+        regex=r"^(?P<user_id>[0-9]+)/follow/$", 
+        view=views.FollowUser.as_view(), 
+        name="follow_user"
+    ),
+    url(
+        regex=r"^(?P<user_id>[0-9]+)/unfollow/$", 
+        view=views.UnFollowUser.as_view(), 
+        name="unfollow_user"
+    ),
+    url(
+        regex=r"^(?P<username>\w+)/$",
+        view=views.UserProfile.as_view(),
+        name='user_profile'
+    ),
+    url(
+        regex=r"^(?P<username>\w+)/followers/$",
+        view=views.UserFollowers.as_view(),
+        name='user_followers'
+    ),
+    url(
+        regex=r"^(?P<username>\w+)/following/$",
+        view=views.UserFollowing.as_view(),
+        name='user_following'
     ),
 ]
