59dfa47fa1c4a91464ea989a2b0411d9a72f44e6
kirim-Lee <uthi1004@gmail.com>
2018-06-13 23:02:13 +0900

    first auth

---
+++ frontend/src/components/Auth/index.js
@@ -0,0 +1,12 @@
+import {connect} from "react-redux";
+import Container from './container';
+
+// add all the actiosn for:
+// Log in
+// Sign up
+// Recover password
+// Check username
+// Check password
+// Check email
+
+export default connect()(Container);
