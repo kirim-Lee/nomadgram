46c3224beaf2ea786e93a2a6788e2569d0be15c8
kirim-Lee <uthi1004@gmail.com>
2018-06-13 13:16:05 +0900

    setting for frontend

---
+++ frontend/src/components/App/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>tada</div>
+    );
+  }
+}
+
+export default App;
---
+++ frontend/src/translations.js
@@ -0,0 +1,10 @@
+ const translations={
+    es:{
+        "Login in":"Iniciar Ses16n"
+    },
+    kr:{
+        "Login in":"로그인을 해주세요"
+    }
+}
+
+export {translations };
